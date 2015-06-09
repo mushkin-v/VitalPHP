ee1a24253c62eb7645dcd004e961fb0c72a33337
mushkin-v <mushkin.v@gmail.com>
2015-06-09 17:34:09 +0300

    add autoload

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__.'/vendor/symfony/class-loader/Psr4ClassLoader.php';
+
+use Symfony\Component\ClassLoader\Psr4ClassLoader;
+
+$loader = new Psr4ClassLoader();
+
+$loader->addPrefix('Symfony\\Component\\HttpFoundation', __DIR__.'/vendor/symfony/http-foundation');
+
+$loader->register();
